ac9e99b22c64dd26c3c79aecb65094410d814d32
[ Taha. Dostifam ] <mr.tahadostifam@gmail.com>
2024-12-18 23:55:08 +0330

    .

---
+++ csharp/samples/Models.cs
@@ -0,0 +1,9 @@
+namespace Models
+{
+    public class Person
+    {
+        public required String FullName { get; set; }
+        public required String Username { get; set; }
+        public required Int64 Age { get; set; }
+    }
+}
--- csharp/tutorial /Hello.cs
+++
@@ -1,9 +0,0 @@
-namespace Hello
-{
-    class Hello
-    {
-        static void SayHello() {
-            System.Console.WriteLine("Hi!!!");
-        }
-    }
-}
