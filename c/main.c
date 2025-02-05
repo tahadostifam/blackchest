d1708d7bd142e29e17ab3d6db3f64b8dd9cd5c11
[ Taha. Dostifam ] <mr.tahadostifam@gmail.com>
2025-02-05 13:51:41 +0330

    .

--- c/main.c
+++ c/main.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
+#include <math.h>
 
 int main()
 {
+    double x = sqrt(9);
+    printf("%f\n", x);
 
     return 0;
 }
--- lua/main.lua
+++ lua/main.lua
@@ -1,18 +1,3 @@
--- Define a variable
-local x = 10
-
--- Nested if-else blocks
-if x > 0 then
-  print("x is positive")
-  if x < 10 then
-    print("x is less than 10")
-  elseif x == 10 then
-    print("x is equal to 10")
-  else
-    print("x is greater than 10")
-  end
-elseif x < 0 then
-  print("x is negative")
-else
-  print("x is zero")
+for i = 1, 10, 1 do
+  print(i)
 end
